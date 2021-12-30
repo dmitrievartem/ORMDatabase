c0200ec614dc7a04c14e2f5c5cf5698b218b5a32
Артем Дмитриев <artem2733@yandex.ru>
2021-12-30 15:20:52 +0300

    fix npe in text fields

--- src/main/java/ormdatabase/controller/AddRecordController.java
+++ src/main/java/ormdatabase/controller/AddRecordController.java
@@ -174,24 +174,24 @@ public class AddRecordController extends SceneSwitcher {
         List<TextField> onlyTextFields = List.of(nameField, carField, cityField, authorField);
         for (TextField textField : onlyTextFields) {
             textField.textProperty().addListener((observable, oldValue, newValue) -> {
-                if (!newValue.matches("[(][)]\\s\\p{L}*")) {
+                if (Objects.nonNull(newValue) && !newValue.matches("[(][)]\\s\\p{L}*")) {
                     textField.setText(newValue.replaceAll("[^[(][)]\\s\\p{L}*]", ""));
                 }
-                if (newValue.length() > 50) {
+                if (Objects.nonNull(newValue) && newValue.length() > 50) {
                     textField.setText(newValue.substring(0, 50));
                 }
             });
         }
         phoneField.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
+            if (Objects.nonNull(newValue) && !newValue.matches("\\d*")) {
                 phoneField.setText(newValue.replaceAll("[^\\d]", ""));
             }
-            if (newValue.length() > 11) {
+            if (Objects.nonNull(newValue) && newValue.length() > 11) {
                 phoneField.setText(newValue.substring(0, 11));
             }
         });
         commentField.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue.length() > 500) {
+            if (Objects.nonNull(newValue) && newValue.length() > 500) {
                 commentField.setText(newValue.substring(0, 500));
             }
         });
